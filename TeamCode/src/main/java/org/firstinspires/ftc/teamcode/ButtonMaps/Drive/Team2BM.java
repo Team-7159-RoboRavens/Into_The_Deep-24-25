c9443cf2903936e45caa81c5ac795da0254b2db4
dhanyathav1 <dhanyatha.angai@gmail.com>
2025-07-10 14:21:20 -0700

    Update Team2BM.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/Team2BM.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ButtonMaps/Drive/Team2BM.java
@@ -16,7 +16,7 @@ public class Team2BM extends AbstractButtonMap {
     @Override
     public void loop(IntoTheDeepRobot robot, OpMode opMode) {
         mp = new MotorPowers(0);
-        double servoPosition = 0;
+        double servoPosition = 0.0;
         double triggerMultiplier = 0.75;
 
 //forward
@@ -57,18 +57,18 @@ public class Team2BM extends AbstractButtonMap {
 
 //right turn
         else if (opMode.gamepad1.right_stick_x > 0.1) {
-            mp = new MotorPowers(opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    -opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    -opMode.gamepad1.right_stick_x * triggerMultiplier);
+            mp = new MotorPowers(opMode.gamepad1.right_stick_x,
+                    -opMode.gamepad1.right_stick_x,
+                    opMode.gamepad1.right_stick_x,
+                    -opMode.gamepad1.right_stick_x);
             opMode.telemetry.addLine(" (right turn)active");
         }
 //left turn
         else if (opMode.gamepad1.right_stick_x < -0.1) {
-            mp = new MotorPowers(-opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    -opMode.gamepad1.right_stick_x * triggerMultiplier,
-                    opMode.gamepad1.right_stick_x * triggerMultiplier);
+            mp = new MotorPowers(-opMode.gamepad1.right_stick_x,
+                    opMode.gamepad1.right_stick_x,
+                    -opMode.gamepad1.right_stick_x,
+                    opMode.gamepad1.right_stick_x);
             opMode.telemetry.addLine("(left turn)active");
         }
 //servo open
@@ -80,6 +80,7 @@ public class Team2BM extends AbstractButtonMap {
         else if (opMode.gamepad1.left_trigger > 0.1) {
             servoPosition = 0.1;
         }
+        mp = new MotorPowers(mp.leftFront, mp.rightFront, mp.leftBack, mp.rightBack);
 
         robot.servo1.setPosition(servoPosition);
 
